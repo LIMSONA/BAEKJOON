06ea12f4b0ddb70ed8b322d46bda55fac47f468f
또방 :-) <93628834+LIMSONA@users.noreply.github.com>
2021-11-18 22:59:25 +0900

    1118_1

---
+++ if(kakao)2021/Day3 내맘대로 여행패스.py
@@ -0,0 +1,19 @@
+# 기대했던 8박9일 신혼여행
+
+Honeymooon=[[] for _ in range(9)]
+Love_it=[가우디 투어, 스페인 맛있는 음식, 스페인 특산품 쇼핑, 스페인 추억 사진찍기]
+
+Day = 0
+COVID_19 = a_lot_of_virus
+
+for something in Love_it
+    Honeymooon[Day].append(something)
+    Day += 1
+    COVID_19 -= Efforts to reduce COVID_19
+    
+while True:
+    if Covid_19 == 0:
+        print(Honeymooon)
+    
+    else:
+        print('조금만 기다려보자ㅠㅠ')
