0e710cf8b224416123b34bddb47f2f9a41bd1cb9
또방 :-) <93628834+LIMSONA@users.noreply.github.com>
2021-11-21 22:10:33 +0900

    1121_1

--- BAEKJOON/2805번 나무자르기.py
+++ BAEKJOON/2805번 나무자르기.py
@@ -1,29 +1,28 @@
-n,m=map(int,input().split())
-woods=list(map(int,input().split())) #나무 값들
+import sys
 
-start=min(woods)
+n,m=map(int,sys.stdin.readline().split())
+woods=list(map(int,sys.stdin.readline().split())) #나무 값들
+
+start=0
 end=max(woods)
 
-result=[]   #정답 후보리스트
+result=0   #정답 후보
 
 while start<=end :
     
     mid=(start+end)//2   #중간값
     get=0 #자르고 남은 나무
 
-    for i in woods:
-        if i>mid:
-            get += i-mid
+    
+    get=sum(i-mid if i>mid else 0 for i in woods)
+    # for i in woods:
+    #     if i>mid:
+    #         get += i-mid
         
     if get < m:
-        end= mid-1
-        
-    elif get == m:
-        result.append(mid)
-        break
-    
-    elif get > m:
-        result.append(mid)
+        end= mid-1  
+    else: # get >= m:
+        result = mid
         start = mid+1
 
-print(max(result))
+print(result)
