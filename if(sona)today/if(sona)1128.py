8012505aa5d8a328c1ef4519a308a2094e51dbd4
또방 :-) <93628834+LIMSONA@users.noreply.github.com>
2021-11-28 22:47:10 +0900

    1128_2

--- if(sona)today/if(sona)1128.py
+++ if(sona)today/if(sona)1128.py
@@ -1,13 +1,16 @@
 from collections import deque
 
+#2020yr 좋아하는 차
 my_favorite_car = [['BMW_MINI_COOPER', 'HYUNDAI_NEXO', 'GENESIS_GV80']]
 in_my_mind = deque(my_favorite_car)
 
+#2021 서울모빌리티 가기 전
 these_days=[['HYUNDAI_CASPER', 'BMW_IX', 'PORSCHE_TARGA'],\
+#2021 서울 모빌리티 갔다 온 후
             ['MERCEDES_BENZ_EQS']]
 
 for i in these_days:
     in_my_mind.append(i)
     in_my_mind.popleft()
     
-    print('my_favorite_cars는', in_my_mind)
+    print('요즘 my_favorite_cars는', in_my_mind)
