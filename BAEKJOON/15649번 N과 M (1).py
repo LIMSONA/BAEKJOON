070e00de91ed8e8929cef948dbfde355e3d5c1c5
또방 :-) <93628834+LIMSONA@users.noreply.github.com>
2021-11-28 21:46:58 +0900

    1128_1

--- BAEKJOON/15649번 N과 M (1).py
+++ BAEKJOON/15649번 N과 M (1).py
@@ -1,10 +1,9 @@
+#순열 라이브러리 알고있으면 겁나 쉬운 문제!
+
 import itertools
 
 m,n=map(int,input().split())
-
-result=itertools.combinations(range(1,m+1),n)
+result=itertools.permutations(range(1,m+1),n)
 
 for i in result:
-    
-    
-    
+    print(*i)
