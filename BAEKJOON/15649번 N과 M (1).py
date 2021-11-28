d9ae699e32025c334c077b048695bfa06ade2416
또방 :-) <93628834+LIMSONA@users.noreply.github.com>
2021-11-28 22:11:27 +0900

    1128_1

--- BAEKJOON/15649번 N과 M (1).py
+++ BAEKJOON/15649번 N과 M (1).py
@@ -1,4 +1,5 @@
 #순열 라이브러리 알고있으면 겁나 쉬운 문제!
+# m개 중 n개 뽑기! 순서상관있음!
 
 import itertools
 
@@ -6,4 +7,4 @@ m,n=map(int,input().split())
 result=itertools.permutations(range(1,m+1),n)
 
 for i in result:
-    print(*i)
+    print(*i) #안에 든거가 하나씩 나와야하니까
