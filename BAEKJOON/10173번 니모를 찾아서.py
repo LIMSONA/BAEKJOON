f3cb9e569b279293a15a43c193b7a1ebd37e75f5
또방 :-) <93628834+LIMSONA@users.noreply.github.com>
2021-12-08 14:32:21 +0900

    1208_1

--- BAEKJOON/10173번 니모를 찾아서.py
+++ BAEKJOON/10173번 니모를 찾아서.py
@@ -1,8 +1,9 @@
 import re
-
 while True:
     sen=input()
-    if sen=='EOI': break
-    p=re.findall('.nemo.',sen.lower())
+    if sen=='EOI': break #스톱 식 끝내기
+    
+    #소문자로 바꾼 문장 중 니모찾기
+    p=re.findall('.nemo.',sen.lower()) 
     if len(p)!=0: print("Found")
     else: print("Missing")
