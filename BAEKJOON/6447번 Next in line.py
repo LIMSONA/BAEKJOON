8fefeef55f458138ff4e2e85a5d46ebfdcc3ddfe
또방 :-) <93628834+LIMSONA@users.noreply.github.com>
2021-12-09 23:06:45 +0900

    1209_4

---
+++ BAEKJOON/6447번 Next in line.py
@@ -0,0 +1,7 @@
+#둘째막내나이차 == 첫째둘째 나이차
+#입력: 첫번째 줄 막내y 둘째줄 둘쨰m
+# result - m = m-y
+# result = m-y+m = 2m-y
+y=int(input())
+m=int(input())
+print(2*m-y)
---
+++ BAEKJOON/7287번 등록.py
@@ -0,0 +1,2 @@
+print('121')
+print('lightsn52')
