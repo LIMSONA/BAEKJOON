623a9eba696ad97e370cfac3e9d0c4791bc788fd
또방 :-) <93628834+LIMSONA@users.noreply.github.com>
2021-12-09 23:06:51 +0900

    1209_5

---
+++ BAEKJOON/5522번 카드게임.py
@@ -0,0 +1,5 @@
+cnt=0
+for _ in range(5):
+    n=int(input())
+    cnt+=n
+print(cnt)
---
+++ BAEKJOON/5554번 심부름 가는 길.py
@@ -0,0 +1,7 @@
+cnt=0
+for _ in range(4):
+    n=int(input())
+    cnt+=n
+
+print(cnt//60) #min
+print(cnt%60) #sec
