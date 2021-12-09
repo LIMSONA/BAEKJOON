f7e912051c806be3b65ddb3c49f3cc1cb9b1502e
또방 :-) <93628834+LIMSONA@users.noreply.github.com>
2021-12-09 23:06:37 +0900

    1209_3

---
+++ BAEKJOON/8370번 Plane.py
@@ -0,0 +1,4 @@
+# 비즈니스: n1*k1  이코노미: n2*k2
+
+n=list(map(int,input().split(" ")))
+print(n[0]*n[1]+n[2]*n[3])
---
+++ BAEKJOON/8437번 Julka.py
@@ -0,0 +1,7 @@
+a=int(input()) #두소녀 합
+b=int(input()) #두소녀 차
+
+#k의 사과수
+print((a+b)//2)
+#n의 사과수
+print((a-b)//2)
