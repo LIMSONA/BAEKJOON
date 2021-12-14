0b8ba5f91cbfd0c4458e700c7aa3789115d5debb
또방 :-) <93628834+LIMSONA@users.noreply.github.com>
2021-12-15 00:52:56 +0900

    1213_2

--- BAEKJOON/10491번 Quite a problem.py
+++ BAEKJOON/10491번 Quite a problem.py
@@ -1,7 +1,6 @@
 #실패=========================
 
 import re
-
 while True:
     a=input()
     p=re.match('problem', a.lower())
@@ -11,10 +10,12 @@ while True:
     
 #성공===================================== 
 import re
-while True:
+while True: #입력이 반복될 거니까
     try:
         a=input()
-        p=re.match('.*problem+', a.lower())
+        #대소문자 구분하지 않도록 lower()하거나 또는
+        #re.match('.*problem', a, re.I)로 대소문자 무시한다.
+        p=re.match('.*problem', a.lower())
         if p: print('yes')
         else: print('no')
-    except EOFError: break
+    except EOFError: break #입력이 끝나면 break
