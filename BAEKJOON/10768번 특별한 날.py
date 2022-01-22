575b85dab3e5f17f0ca2ffc6b4fdd4ee5b5e0e4a
또방 :-) <93628834+LIMSONA@users.noreply.github.com>
2022-01-22 23:34:44 +0900

    0122_4

---
+++ BAEKJOON/10768번 특별한 날.py
@@ -0,0 +1,10 @@
+m=int(input())
+d=int(input())
+
+if m==1: print("Before")
+elif m==2:
+    if d<18: print("Before")
+    elif d==18: print("Special")
+    elif d>18: print("After")
+else:
+    print("After")
---
+++ BAEKJOON/10797번 10부제.py
@@ -0,0 +1,5 @@
+t=int(input())
+subject=list(map(int,input().split()))
+
+a= subject.count(t)
+print(a)
