5b5ab4b8506bf9bcb5f9830ff4c829dc9e694caa
또방 :-) <93628834+LIMSONA@users.noreply.github.com>
2022-07-07 15:16:04 +0900

    0707_3

---
+++ BAEKJOON/4999번 아!.py
@@ -0,0 +1,15 @@
+jh= input()
+doctor= input()
+
+if len(jh) >= len(doctor): print("go")
+else: print("no")
+
+
+
+#=====다른 방법=======
+
+jh= input()
+doctor= input()
+
+if jh.count("a") >= doctor.count("a"): print("go")
+else: print("no")
